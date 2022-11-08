5938df4d1a2dbda09859b27c4afc11fe6d4f5b10
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-11-08 20:15:38 +0530

    Create operation1.java

---
+++ operation1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[]= {2,4,5,6,4};
+		int arr1[]= {2,34,56,76};
+		String s[] = {"maya","avina","jiya"};
+		
+		 System.out.println(Arrays.equals(arr,arr1));
+		 System.out.println(Arrays.toString(arr));
+		 List<String> list = Arrays.asList(s);
+		 System.out.println(list);
+	}
+}
