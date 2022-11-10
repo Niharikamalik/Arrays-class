76ad9046e8dd77c7be2d519369d1b5448dbe27e2
Niharika Malik <73269292+Niharikamalik@users.noreply.github.com>
2022-11-10 13:13:45 +0530

    Create Binary_search.java

---
+++ Binary_search.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main
+{
+  public static void main (String[]args)
+  {
+    /* code */
+    int arr[] = { 2, 3, 4, 5, 6, 1 };
+    System.out.println(Arrays.toString(arr));
+    System.out.println (Arrays.binarySearch (arr, 3));
+  }
+}
